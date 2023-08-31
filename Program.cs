0d52416920f59895259085105656b4ab9970f16c
Wellisson Reis <64636947+wellissonreis@users.noreply.github.com>
2023-08-31 01:37:55 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+using ConsumirAPI.Dtos;
+using ConsumirAPI.Mappins;
+using ConsumirAPI.Models;
+using Octokit;
+using System.Net.Http.Headers;
+using ProductHeaderValue = Octokit.ProductHeaderValue;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddAutoMapper(typeof(ReposMapper));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
